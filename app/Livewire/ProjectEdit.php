c4cfa1579e7b7bff23f6e7e0324eb96a9a852de8
thbappy7706 <tanvir15-7706@diu.edu.bd>
2023-11-12 23:16:38 +0600

    stubs

---
+++ app/Livewire/ProjectEdit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ProjectEdit extends Component
+{
+
+       public function mount()
+       {
+
+       }
+
+    public function render()
+    {
+        return view('livewire.project-edit');
+    }
+}
