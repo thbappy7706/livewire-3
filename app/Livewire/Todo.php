0510309ef5c4adce21941d1f9261fba689ec6c63
thbappy7706 <tanvir15-7706@diu.edu.bd>
2023-11-13 19:22:27 +0600

    --

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -8,10 +8,23 @@ use Livewire\Component;
 class Todo extends Component
 {
 
-    public $todoItem;
-    public $todos = [
-        'Eating Dinner', 'Sleep', 'Code'
-    ];
+    public $todoItem = '';
+    public $todos = [];
+
+    public function mount()
+    {
+        $this->todos = ['Eating Dinner', 'Sleep', 'Code'];
+    }
+
+    public function updatedTodoItem($val)
+    {
+        $this->todoItem = strtoupper($val);
+    }
+
+//    public function updated($pro, $val)
+//    {
+//        $this->$pro = strtoupper($val);
+//    }
 
     public function render(): View
     {
--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -1,7 +1,8 @@
 <div>
 
     <form wire:submit="add">
-        <input type="text" wire:model.live.debounce.5ms="todoItem">
+{{--        <input type="text" wire:model.live.debounce.5ms="todoItem">--}}
+        <input type="text" wire:model="todoItem">
         <span>Current todo: {{$todoItem}}</span>
         {{--    <button wire:click="add" type="button">Add Todo</button>--}}
         <button type="submit">Add Todo</button>
